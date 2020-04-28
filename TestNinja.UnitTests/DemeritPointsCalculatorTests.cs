6f4488f34a0d7ce6fc8b7a888ebdc8b3112277e0
RaulLH <raullopezhernando@gmail.com>
2020-04-28 17:56:04 +0200

    Agregar archivos de proyecto.

---
+++ TestNinja.UnitTests/DemeritPointsCalculatorTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class DemeritPointsCalculatorTests
+    {
+        [Test]
+        [TestCase(-1)]
+        [TestCase(301)]
+        public void CalculateDemeritPoints_SpeedIsOutOfRange_ThrowArgumentOutOfRangeException(int speed)
+        {
+            var calculator = new DemeritPointsCalculator();
+            //We need to use the lambda expression because we are testing results that throw exceptions
+            Assert.That(() => calculator.CalculateDemeritPoints(speed), Throws
+                .Exception.TypeOf<ArgumentOutOfRangeException>());
+        }
+
+        [Test]
+        [TestCase(0, 0)]
+        [TestCase(64, 0)]
+        [TestCase(65, 0)]
+        [TestCase(66, 0)]
+        [TestCase(70, 1)]
+        [TestCase(75, 2)]
+
+        //In the "Test Cases" the first parameter refers to "Speed" 
+        //and the second parameter refers to "Expected Result"
+        public void CalculateDemeritPoints_WhenCalled_ReturnDemeritpoints(int speed,int expectedResult)
+        {
+            var calculator = new DemeritPointsCalculator();
+            var result = calculator.CalculateDemeritPoints(speed);
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
---
+++ TestNinja.UnitTests/StackTests.cs
@@ -0,0 +1,100 @@
+﻿using NUnit.Framework;
+using System;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    class StackTests
+    {
+        [Test]
+        public void Push_ArgIsNull_ThrowArgNullException()
+        {
+            var stack = new Stack<string>();
+            Assert.That(() => stack.Push(null), Throws.TypeOf<ArgumentNullException>());
+        }
+
+        [Test]
+        public void Push_ValidArg_AddTheObjectToTheStack()
+        {
+            var stack = new Stack<string>();
+            stack.Push("a");
+            Assert.That(stack.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Count_EmptyStack_ReturnZero()
+        {
+            var stack = new Stack<string>();
+            Assert.That(stack.Count, Is.EqualTo(0));
+        }
+
+
+        [Test]
+        public void Pop_EmptyStack_ThrowInvalidOperationException()
+        {
+            var stack = new Stack<string>();
+            Assert.That(() => stack.Pop(), Throws.TypeOf<InvalidOperationException>());
+        }
+
+        [Test]
+        public void Pop_StackWithAfewObjects_ReturnObjectOnTheTop() 
+        {
+            // Arrange 
+            var stack = new Stack<string>();
+            stack.Push("a");
+            stack.Push("b");
+            stack.Push("c");
+
+            //Act
+            var result = stack.Pop();
+            //Assert
+            Assert.That(result, Is.EqualTo("c"));
+        }
+
+        [Test]
+        public void Pop_StackWithAfewObjects_RemovesObjectOnTheTop() 
+        {
+            var stack = new Stack<string>();
+            stack.Push("a");
+            stack.Push("b");
+            stack.Push("c");
+
+            //Act
+            stack.Pop();
+            //Assert
+            Assert.That(stack.Count,Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Peek_EmptyStack_ThrowInvalidOperationException() 
+        {
+            var stack = new Stack<string>();
+            Assert.That(() => stack.Peek(), Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void Peek_StackWithObjects_ReturnObjectsOnTheTopOfTheStack()
+        {
+            var stack = new Stack<string>();
+            stack.Push("a");
+            stack.Push("b");
+            stack.Push("c");
+            var result = stack.Peek();
+            Assert.That(result, Is.EqualTo("c"));
+        }
+
+        [Test]
+        public void Peek_StackWithObjects_DoesNotRemoveTheObjectOnTopOfTheStack()
+        {
+            var stack = new Stack<string>();
+            stack.Push("a");
+            stack.Push("b");
+            stack.Push("c");
+
+            stack.Pop();
+            
+            Assert.That(stack.Count, Is.EqualTo(3));
+        }
+    }
+}
