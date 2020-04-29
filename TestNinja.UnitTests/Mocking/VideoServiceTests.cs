51f82e0e9ac6a4612f16b06b40e99f1298e5f5db
RaulLH <raullopezhernando@gmail.com>
2020-04-29 13:22:23 +0200

    Primer Commit. UnitTest C#

---
+++ TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using Moq;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class VideoServiceTests
+    {
+        [Test]
+        public void ReadVideoTitle_EmptyFile_ReturnError()
+        {
+            var fileReader= new Mock<IFileReader>();
+            fileReader.Setup(fr => fr.Read("video.txt")).Returns("");
+
+
+            var service = new VideoService(new FakeFileReader());
+
+            var result = service.ReadVideoTitle();
+
+            Assert.That(result, Does.Contain("error").IgnoreCase);
+        }
+    }
+}
